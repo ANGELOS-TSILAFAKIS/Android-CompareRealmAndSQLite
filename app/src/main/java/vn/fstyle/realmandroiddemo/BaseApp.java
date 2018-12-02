43d63d57f637e94ed24b7b40a69c66d5b2be8f17
A-PC-A <AGGELOS.TSILAFAKIS.GITHUB@gmail.com>
2018-12-02 19:52:46 +0200

    simple save.

--- app/src/main/java/vn/fstyle/realmandroiddemo/BaseApp.java
+++ app/src/main/java/vn/fstyle/realmandroiddemo/BaseApp.java
@@ -15,11 +15,23 @@ public class BaseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // The realm file will be located in Context.getFilesDir() with name "default.realm"
-        RealmConfiguration config = new RealmConfiguration.Builder(this)
-                .deleteRealmIfMigrationNeeded()
-                .build();
+//        // The realm file will be located in Context.getFilesDir() with name "default.realm"
+//        RealmConfiguration config = new RealmConfiguration.Builder(this)
+//                .deleteRealmIfMigrationNeeded()
+//                .build();
+//
+//        Realm.setDefaultConfiguration(config);
 
-        Realm.setDefaultConfiguration(config);
+
+        Realm.init(this);
+        RealmConfiguration realmConfiguration =
+                new RealmConfiguration.Builder()
+                        .deleteRealmIfMigrationNeeded()
+                        .build();
+
+        // Deletes the realm
+        // Use when you want a clean slate for dev
+//        Realm.deleteRealm(realmConfiguration);
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 }
